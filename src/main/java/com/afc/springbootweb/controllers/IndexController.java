561317f9cf0d66e7adfde090af7c3923846ac7a2
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-12-04 22:48:35 -0500

    implementing user-profile

--- src/main/java/com/afc/springbootweb/controllers/IndexController.java
+++ src/main/java/com/afc/springbootweb/controllers/IndexController.java
@@ -1,12 +1,11 @@
 package com.afc.springbootweb.controllers;
 
+import com.afc.springbootweb.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
 
 @Controller
 @RequestMapping("/app")
@@ -16,5 +15,17 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("title", "Hello buddy!");
         return "index";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model){
+
+        User user = new User();
+        user.setName("Andrea");
+        user.setLastName("Fuentes");
+
+        model.addAttribute("user", user);
+        model.addAttribute("title", "User profile".concat(user.getName()));
+        return "profile";
     }
 }
