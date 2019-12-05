8957fc6eb55c04e14e5b88eb8c55ef60daff6a0c
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-12-04 22:06:19 -0500

    add controller request mapping

--- src/main/java/com/afc/springbootweb/controllers/IndexController.java
+++ src/main/java/com/afc/springbootweb/controllers/IndexController.java
@@ -3,14 +3,16 @@ package com.afc.springbootweb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-    @GetMapping({"/index", "/", "/home"})
+    @GetMapping({"/index", "/", "", "/home"})
     public String index(Model model) {
         model.addAttribute("title", "Hello buddy!");
         return "index";
