3a5eba8c2872442919a654fab23ca291e852eb89
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-12-04 21:45:22 -0500

    change ModelMap for map

--- src/main/java/com/afc/springbootweb/controllers/IndexController.java
+++ src/main/java/com/afc/springbootweb/controllers/IndexController.java
@@ -1,15 +1,16 @@
 package com.afc.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"/index", "/", "/home"})
-    public String index(ModelMap model) {
-        model.addAttribute("title", "Hello buddy!");
+    public String index(Map<String, Object> map) {
+        map.put("title", "Hello buddy!");
         return "index";
     }
 }
