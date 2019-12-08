ec43d5b8536e18352f86a77aaa4fb321417045d4
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-12-08 17:33:48 -0500

    list users

--- src/main/java/com/afc/springbootweb/controllers/IndexController.java
+++ src/main/java/com/afc/springbootweb/controllers/IndexController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/app")
@@ -18,14 +21,23 @@ public class IndexController {
     }
 
     @GetMapping("/profile")
-    public String profile(Model model){
+    public String profile(Model model) {
 
         User user = new User();
         user.setName("Andrea");
         user.setLastName("Fuentes");
+        user.setEmail("dev@test.com");
 
         model.addAttribute("user", user);
         model.addAttribute("title", "User profile".concat(user.getName()));
         return "profile";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<User> users = new ArrayList<>();
+        model.addAttribute("title", "users_list");
+        model.addAttribute("users", users);
+            return"list";
     }
 }
