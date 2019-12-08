caace619498f395c464d53b8379be1672ebc4947
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-12-08 18:48:56 -0500

    use ModelAttribute

--- src/main/java/com/afc/springbootweb/controllers/IndexController.java
+++ src/main/java/com/afc/springbootweb/controllers/IndexController.java
@@ -4,6 +4,7 @@ import com.afc.springbootweb.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -36,13 +37,17 @@ public class IndexController {
 
     @GetMapping("/list")
     public String list(Model model) {
+        model.addAttribute("title", "users_list");;
+            return"list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> loadUsers(){
         List<User> users = Arrays.asList(new User("Daniel", "Torres", "dt@test.com"),
                 new User("Juli", "Davila", "jd@test.com"),
                 new User("Mich", "Perez", "mp@test.com"),
                 new User("Juan", "Molina", "jm@test.com"),
-                new User("Rick", "Noulose", "jm@test.com"),);
-        model.addAttribute("title", "users_list");
-        model.addAttribute("users", users);
-            return"list";
+                new User("Rick", "Noulose", "jm@test.com"));
+        return users;
     }
 }
