621a63bff1f0615e9f4895be472e840c1a7e11b9
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-12-09 20:32:41 -0500

    receive param

--- src/main/java/com/afc/springbootweb/controllers/ExampleParamsController.java
+++ src/main/java/com/afc/springbootweb/controllers/ExampleParamsController.java
@@ -10,8 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/params")
 public class ExampleParamsController {
 
+    @GetMapping("/")
+    public String index(){
+
+        return "params/index";
+    }
+
     @GetMapping("/string")
-    public String param(@RequestParam(name = "Text", required = false, defaultValue = "testing") String textVariable, Model model){
+    public String param(@RequestParam(name = "text", required = false, defaultValue = "testing") String textVariable, Model model){
         model.addAttribute("result", "El texto enviado es: " + textVariable);
         return "params/show";
     }
