0c0a317c8f3c08e138bdd6b6dd75a80e4978eee1
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-12-18 21:10:23 -0500

    add new file property and use redirect

---
+++ src/main/java/com/afc/springbootweb/TextPropertiesConfig.java
@@ -0,0 +1,12 @@
+package com.afc.springbootweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:text.properties")
+})
+public class TextPropertiesConfig {
+}
